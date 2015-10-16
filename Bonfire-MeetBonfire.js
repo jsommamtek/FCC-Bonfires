790c9529bdcd022704192a2c24198bd8b32bce8f
Joe Somma <jsomma@millentek.com>
2015-10-16 15:37:29 -0400

    Added a jquery comment to test in CodePen

--- Bonfire-MeetBonfire.js
+++ Bonfire-MeetBonfire.js
@@ -3,7 +3,11 @@ function meetBonfire(argument) {
   // Good luck!
   console.log("you can read this function's argument in the developer tools", argument);
 
-  return true;
+return true;
 }
 
 meetBonfire("You can do this!");
+
+// Make sure you have JQuery lib ref 
+// Tested in CodePen 
+$("#myDiv").html("Hey Bonfire check the js console using dev tools for the log entry!");
